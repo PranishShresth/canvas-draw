ce31350b6396c80deac002d959f7354d0b395665
PranishShresth <yunche64@gmail.com>
2021-09-19 11:23:10 +1000

    react children to node

--- src/context/CanvasContext.tsx
+++ src/context/CanvasContext.tsx
@@ -3,7 +3,7 @@ import { createContext, FunctionComponent } from "react";
 export const canvasContext = createContext(null);
 
 interface Props {
-  children: React.ReactChildren;
+  children: React.ReactNode;
 }
 export const CanvasProvider: FunctionComponent<Props> = (props: Props) => {
   return <main className="mainContainer">{props.children}</main>;
